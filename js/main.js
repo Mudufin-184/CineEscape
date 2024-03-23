d55a1744fba880e9b1749ed2b9cab6ae29c80ca2
Mudufin-184 <129909015+Mudufin-184@users.noreply.github.com>
2024-03-23 10:48:10 -0500

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function panellateral(){
+    let panel = document.getElementById("panel-lateral");
+
+    panel.classList.toggle("active");
+}
+
+function modal() {
+    let modal = document.getElementById("modal-pelicula");
+    modal.classList.toggle("active");
+}
